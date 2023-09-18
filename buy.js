f6be76ab12c9d3cd1a557e9d4b889018767a1467
byungi <zeronin2@gmail.com>
2023-09-18 22:47:22 +0900

    maintain

--- buy.js
+++ buy.js
@@ -1,8 +1,14 @@
 import createCrawler from './lib/createCrawler.js'
 import parseArgvOrExit from './lib/parseArgvOrExit.js'
 import createSlackWebhookSender from './lib/createSlackWebhookSender.js'
+import formatMoney from './lib/formatMoney.js'
 
-const { id, pw, url, amount: amountStr } = parseArgvOrExit({
+const {
+    id,
+    pw,
+    url,
+    amount: amountStr,
+} = parseArgvOrExit({
     id: '동행복권ID',
     pw: '동행복권비번',
     url: '슬랙웹훅주소',
@@ -13,14 +19,26 @@ const send = createSlackWebhookSender(url)
 const crawler = await createCrawler(id, pw)
 
 let $ = await crawler.html({ url: '/userSsl.do?method=myPage' })
+
 const money = Number($('.total_new strong').text().replace(/,/g, ''))
 if (money === 0) {
-    await send('[로또구매] 충전된 돈이 없음... 충전해야 함')
+    await send(
+        `
+[로또구매] 돈이 없어서 구매를 못했어요. 돈을 충전해야 합니다.
+https://www.dhlottery.co.kr/payment.do?method=payment
+    `,
+    )
     process.exit(1)
 }
+
 let amount = Number(amountStr)
 if (amount != (amount = Math.min(amount, money / 1000))) {
-    await send(`[로또구매] 돈이 부족해서 ${amount}장만 삽니다.`)
+    await send(
+        `
+[로또구매] 돈이 부족해서 ${amount}장만 삽니다. 미리 충전하세요
+https://www.dhlottery.co.kr/payment.do?method=payment
+`,
+    )
 }
 
 $ = await crawler.html({ url: 'https://ol.dhlottery.co.kr/olotto/game/game645.do' })
@@ -32,6 +50,7 @@ const json = await crawler.request({
     parse: 'json',
     form: {
         round,
+        direct: 'INTCOM2',
         nBuyAmount: amount * 1000,
         param: JSON.stringify(
             Array(amount)
@@ -40,7 +59,7 @@ const json = await crawler.request({
                     genType: '0',
                     arrGameChoiceNum: null,
                     alpabet: String.fromCharCode(65 + i),
-                }))
+                })),
         ),
         gameCnt: amount,
     },
@@ -48,17 +67,18 @@ const json = await crawler.request({
 
 if (json.result.resultCode !== '100') {
     await send(`
-[로또구매] 구매실패함 
-응답: ${JSON.stringify(json)}
+[로또구매] 구매에 실패했습니다.
+---
+디버그 응답: ${JSON.stringify(json, null, 2)}
+---
 `)
     process.exit(1)
 }
 
 await send(`
-[로또구매] 
-${amount}장 구매 했습니다. 
+[로또구매] ${amount}장 구매 했습니다. 
 구매날짜: ${json.result.issueDay}
 구매한 로또번호: 
-${json.result.arrGameChoiceNum.map(str => `\t${str.slice(0, -1).split('|').slice(1).join(',')}`).join('\r\n')}
-남은돈: ${money - amount * 1000}원
+${json.result.arrGameChoiceNum.map(str => `\t - ${str.slice(0, -1).split('|').slice(1).join(',')}`).join('\r\n')}
+남은돈: ${formatMoney(money - amount * 1000)}원
 `)
--- check.js
+++ check.js
@@ -1,6 +1,7 @@
 import parseArgvOrExit from './lib/parseArgvOrExit.js'
 import createCrawler from './lib/createCrawler.js'
 import createSlackWebhookSender from './lib/createSlackWebhookSender.js'
+import formatMoney from './lib/formatMoney.js'
 
 const { id, pw, url } = parseArgvOrExit({ id: '동행복권ID', pw: '동행복권비번', url: '슬랙웹훅주소' })
 const crawler = await createCrawler(id, pw)
@@ -22,9 +23,16 @@ const buys = $('table.tbl_data')
     .toArray()
 
 const send = createSlackWebhookSender(url)
-await send(`
+await send(
+    `
 [로또당첨현황]
-${buys.map(o => `\t-구매날짜: ${o.date} | 수량: ${o.amount} | 결과: ${o.result}`).join('\r\n')}
+${
+    buys.length
+        ? buys.map(o => `\t-구매날짜: ${o.date} | 수량: ${o.amount} | 결과: ${o.result}`).join('\r\n')
+        : '\t- 구매기록 없음'
+}
 
-(이제 ${money}원 남아있음..)
-`)
+이제 ${formatMoney(money)}원 남았어요
+${money <= 5_000 ? '돈이 별로 없네요. 충전해야 합니다.' : ''}
+`.trim(),
+)
